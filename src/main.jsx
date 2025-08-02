63fe8c02d59cf51e44c55e7e5ad792dcade88cf3
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-08-02 19:51:40 +0530

    Added About & Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
