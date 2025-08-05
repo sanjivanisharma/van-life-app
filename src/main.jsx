70e420cf15898bff01b85c13f1f4573a6dd893c5
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-08-05 17:32:28 +0530

    Added Van & Van details page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
+import "./server"
 
 
 function App() {
@@ -12,12 +15,15 @@ function App() {
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
+          <Link to="/vans">Vans</Link>
           <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
