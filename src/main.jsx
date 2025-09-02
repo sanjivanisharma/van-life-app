75bc7f8cb7b5f32f4210d21f0471533da1c6f212
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-09-02 20:02:52 +0530

    Added NotFound Page

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import HostVanDetail from './pages/Host/HostVanDetail'
 import HostVanInfo from './pages/Host/HostVanInfo'
 import HostVanPricing from './pages/Host/HostVanPricing'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
+import NotFound from './pages/NotFound'
 import "./server"
 
 
@@ -40,6 +41,7 @@ function App() {
               <Route path='photos' element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
