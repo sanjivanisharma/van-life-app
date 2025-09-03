44a947279f288aa4fbcb8dfa15dbc15813a7d5f1
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-09-03 22:24:52 +0530

    Added Login Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router-dom"
+import avatarUrl from "/src/assets/images/avatar-icon.png"
 
 export default function Header() {
     return (
@@ -17,6 +18,12 @@ export default function Header() {
                     to="/vans"
                     className={({isActive}) => isActive ? "active-link" : null}
                 >Vans</NavLink>
+                <Link to="login" className="login-link">
+                    <img 
+                        src={avatarUrl}
+                        className="login-icon"
+                    />
+                </Link>
             </nav>
         </header>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,7 @@ import HostVanInfo from './pages/Host/HostVanInfo'
 import HostVanPricing from './pages/Host/HostVanPricing'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
 import NotFound from './pages/NotFound'
+import Login from './pages/Login'
 import "./server"
 
 
@@ -27,6 +28,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
     <Route index element={<Home />} />
     <Route path='about' element={<About />} />
+    <Route path='login' element={<Login />} />
     <Route
       path='vans'
       element={<Vans />}
